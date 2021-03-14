8daa5d3f3dd9108796694d9619bbdc656a3d7f75
James Foley <jamesrichmondfoley@googlemail.com>
2021-03-14 21:09:46 +0000

    Update code.py
    Added button tick support and example animation functions

--- code.py
+++ code.py
@@ -26,7 +26,7 @@ i2c = busio.I2C(board.GP5, board.GP4)
 device = I2CDevice(i2c, 0x20)
 
 # Define pixels
-pixels = adafruit_dotstar.DotStar(board.GP18, board.GP19, 16, brightness=0.02, auto_write=True)
+pixels = adafruit_dotstar.DotStar(board.GP18, board.GP19, 16, brightness=0.1, auto_write=True)
 
 # Button state storage
 states = {
@@ -59,7 +59,7 @@ def button_states():
 # setup: func, runs when button is initialised, useful for setting a default colour on boot
 # pressed: func, runs when pressed unless an above modifier changes functionality
 # released: func, runs when released unless an above modifier changes functionality
-def handle_button(button, toggle=False, hold=False, hold_delay=0, setup=None, pressed=None, released=None):
+def handle_button(button, toggle=False, hold=False, hold_delay=0, setup=None, pressed=None, released=None, tick=None):
     if states["setup"][button] == 0:
         states["setup"][button] = 1
         if setup:
@@ -71,7 +71,7 @@ def handle_button(button, toggle=False, hold=False, hold_delay=0, setup=None, pr
             if now - states["hold_time"][button] > hold_delay:
                 pressed()
                 states["hold_time"][button] = now
-            
+
     elif states["previous"][button] == 0 and states["current"][button] == 1:
         if toggle and states["toggle"][button] == 0:
             states["toggle"][button] = 1
@@ -84,12 +84,16 @@ def handle_button(button, toggle=False, hold=False, hold_delay=0, setup=None, pr
         else:
             if pressed:
                 pressed()
+                
     elif states["previous"][button] == 1 and states["current"][button] == 0:
         if not toggle:
             if released:
                 released()
         if hold:
             states["hold_time"][button] = 0
+            
+    if tick:
+        tick()
 
 # Cleanly set pixel colour
 def set_pixel(pixel, colour):
@@ -98,7 +102,57 @@ def set_pixel(pixel, colour):
 # Cleanly clear pixel colour
 def clear_pixel(pixel):
     pixels[pixel] = (0, 0, 0)
+
+# Color wheel function lifted from adafruit_dotstar example
+def colorwheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        return (0, 0, 0)
+    if pos < 85:
+        return (255 - pos * 3, pos * 3, 0)
+    if pos < 170:
+        pos -= 85
+        return (0, 255 - pos * 3, pos * 3)
+    pos -= 170
+    return (pos * 3, 0, 255 - pos * 3)
+
+# Example flash handler for button 11
+# Garbage function just to demo flashing a button
+flash_time = 0
+flash_delay = 0.5
+flash_on = 0
+def flash_button_example():
+    now = time.monotonic()
+    global flash_time
+    global flash_on
+        
+    if now - flash_time > flash_delay:
+        if flash_on == 0:
+            set_pixel(11, (255, 0, 255))
+            flash_on = 1
+        else:
+            set_pixel(11, (0, 0, 0))
+            flash_on = 0
+        
+        flash_time = now
+
+# Example rainbow handler for button 11
+# Garbage function just to demo cycling a button through a rainbow
+rainbow_time = 0
+rainbow_delay = 0
+rainbow_pos = 0
+def rainbow_button_example():
+    now = time.monotonic()
+    global rainbow_time
+    global rainbow_pos
     
+    if now - rainbow_time > rainbow_delay:
+        set_pixel(11, colorwheel(rainbow_pos))
+        rainbow_pos = (rainbow_pos + 2) if rainbow_pos < 253 else 0
+        
+        rainbow_time = now
+
 # Helper function to make button programming less painful, holds information for buttons
 def button_action(button, action):
     if button == 1:
@@ -149,6 +203,13 @@ def button_action(button, action):
         elif action == "pressed":
             cc.send(ConsumerControlCode.SCAN_NEXT_TRACK)
             set_pixel(button, (51, 153, 255))
+            
+    elif button == 11:
+        if action == "tick":
+            if states["current"][11] == 0:
+                flash_button_example()
+            else:
+                rainbow_button_example()
 
     elif button == 12:
         if action == "setup" or action == "released":
@@ -168,22 +229,23 @@ def button_action(button, action):
 while True:
     # Get the state right now
     states["current"] = button_states()
+    
+    hold_buttons = [2, 3, 11]
+    toggle_buttons = [12]  
 
     # Set up buttons
     # Probably a little overcomplicated, but allows a function to be mapped to button events
     for i in range(0, 16):
         handle_button(
             i,
-            hold=i == 2 or i == 3,
-            hold_delay=0.2 if i == 2 or i == 3 else 0,
-            toggle=i == 12,
+            hold=i in hold_buttons,
+            hold_delay=0.2 if i in hold_buttons else 0,
+            toggle=i in toggle_buttons,
             setup=lambda: button_action(i, "setup"),
             pressed=lambda: button_action(i, "pressed"),
-            released=lambda: button_action(i, "released")
+            released=lambda: button_action(i, "released"),
+            tick=lambda: button_action(i, "tick")
         )
 
     # Store the state as previous ready for next loop
     states["previous"] = states["current"]
-
-    # Prevent rapid double hits from quick presses
-    time.sleep(0.02)
