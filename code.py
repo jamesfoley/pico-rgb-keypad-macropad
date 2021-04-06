bd953a2f7d46669dc05ba41e463864dabe50c951
James Foley <jamesrichmondfoley@googlemail.com>
2021-04-06 12:27:11 +0100

    Support pico on keybow

--- code.py
+++ code.py
@@ -1,55 +1,73 @@
 import time
 import board
-import busio
 import digitalio
 import usb_hid
 import adafruit_dotstar
-
-from adafruit_bus_device.i2c_device import I2CDevice
 
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keycode import Keycode
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 
-from digitalio import DigitalInOut, Direction, Pull
-cs = DigitalInOut(board.GP17)
-cs.direction = Direction.OUTPUT
-cs.value = 0
-
 # Define keyboard
 kbd = Keyboard(usb_hid.devices)
 cc = ConsumerControl(usb_hid.devices)
 
-# Define i2c device
-i2c = busio.I2C(board.GP5, board.GP4)
-device = I2CDevice(i2c, 0x20)
-
 # Define pixels
-pixels = adafruit_dotstar.DotStar(board.GP18, board.GP19, 16, brightness=0.1, auto_write=True)
+pixels = adafruit_dotstar.DotStar(board.GP2, board.GP3, 12, brightness=0.1, auto_write=True)
+
+pixel_map = {
+    0: 8,
+    1: 4,
+    2: 0,
+    3: 9,
+    4: 5,
+    5: 1,
+    6: 10,
+    7: 6,
+    8: 2,
+    9: 11,
+    10: 7,
+    11: 3,
+}
+
+button_count = 12
 
 # Button state storage
 states = {
-    "setup": [0] * 16,
-    "current": [0] * 16,
-    "previous": [0] * 16,
-    "toggle": [0] * 16,
-    "hold_time": [0] * 16
+    "setup": [0] * button_count,
+    "current": [0] * button_count,
+    "previous": [0] * button_count,
+    "toggle": [0] * button_count,
+    "hold_time": [0] * button_count
 }
+
+button_gpio_map = {
+    0: board.GP14,
+    1: board.GP17,
+    2: board.GP16,
+    3: board.GP12,
+    4: board.GP18,
+    5: board.GP11,
+    6: board.GP10,
+    7: board.GP26,
+    8: board.GP9,
+    9: board.GP27,
+    10: board.GP8,
+    11: board.GP7,
+}
+
+buttons = {}
+
+for key, value in button_gpio_map.items():
+    buttons[key] = digitalio.DigitalInOut(value)
+    buttons[key].switch_to_input(pull=digitalio.Pull.UP)
 
 # Function to read button states
 def button_states():
-    state = [0] * 16
-    with device:
-        device.write(bytes([0x0]))
-        result = bytearray(2)
-        device.readinto(result)
-        b = result[0] | result[1] << 8
-        for i in range(0, 16):
-            if not (1 << i) & b:
-                state[i] = 1
-            else:
-                state[i] = 0
+    state = [0] * button_count
+    for key, value in buttons.items():
+        state[key] = 0 if value.value else 1
     return state
 
 # Function to handle button events
@@ -84,158 +102,98 @@ def handle_button(button, toggle=False, hold=False, hold_delay=0, setup=None, pr
         else:
             if pressed:
                 pressed()
-                
+
     elif states["previous"][button] == 1 and states["current"][button] == 0:
         if not toggle:
             if released:
                 released()
         if hold:
             states["hold_time"][button] = 0
-            
+
     if tick:
         tick()
 
-# Cleanly set pixel colour
-def set_pixel(pixel, colour):
-    pixels[pixel] = colour
-
-# Cleanly clear pixel colour
-def clear_pixel(pixel):
-    pixels[pixel] = (0, 0, 0)
-
-# Color wheel function lifted from adafruit_dotstar example
-def colorwheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colours are a transition r - g - b - back to r.
-    if pos < 0 or pos > 255:
-        return (0, 0, 0)
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    if pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    pos -= 170
-    return (pos * 3, 0, 255 - pos * 3)
-
-# Example flash handler for button 11
-# Garbage function just to demo flashing a button
-flash_time = 0
-flash_delay = 0.5
-flash_on = 0
-def flash_button_example():
-    now = time.monotonic()
-    global flash_time
-    global flash_on
-        
-    if now - flash_time > flash_delay:
-        if flash_on == 0:
-            set_pixel(11, (255, 0, 255))
-            flash_on = 1
-        else:
-            set_pixel(11, (0, 0, 0))
-            flash_on = 0
-        
-        flash_time = now
-
-# Example rainbow handler for button 11
-# Garbage function just to demo cycling a button through a rainbow
-rainbow_time = 0
-rainbow_delay = 0
-rainbow_pos = 0
-def rainbow_button_example():
-    now = time.monotonic()
-    global rainbow_time
-    global rainbow_pos
-    
-    if now - rainbow_time > rainbow_delay:
-        set_pixel(11, colorwheel(rainbow_pos))
-        rainbow_pos = (rainbow_pos + 2) if rainbow_pos < 253 else 0
-        
-        rainbow_time = now
-
 # Helper function to make button programming less painful, holds information for buttons
 def button_action(button, action):
-    if button == 1:
+    
+    if button == 0:
         if action == "setup" or action == "released":
-            set_pixel(button, (255, 102, 0))
+            set_button_pixel(button, (0, 0, 255))
+            kbd.release(Keycode.SHIFT)
+            kbd.release(Keycode.W)
         elif action == "pressed":
-            cc.send(ConsumerControlCode.MUTE)
-            set_pixel(button, (255, 0, 0))
+            set_button_pixel(button, (51, 153, 255))
+            kbd.press(Keycode.SHIFT)
+            kbd.press(Keycode.W)
 
-    if button == 2:
+    elif button == 2:
         if action == "setup" or action == "released":
-            set_pixel(button, (0, 255, 0))
+            set_button_pixel(button, (0, 0, 255))
         elif action == "pressed":
-            cc.send(ConsumerControlCode.VOLUME_DECREMENT)
-            set_pixel(button, (255, 0, 0))
-
-    elif button == 3:
-        if action == "setup" or action == "released":
-            set_pixel(button, (0, 255, 0))
-        elif action == "pressed":
-            cc.send(ConsumerControlCode.VOLUME_INCREMENT)
-            set_pixel(button, (255, 0, 0))
-
-    elif button == 4:
-        if action == "setup" or action == "released":
-            set_pixel(button, (255, 0, 0))
-        elif action == "pressed":
-            cc.send(ConsumerControlCode.STOP)
-            set_pixel(button, (255, 255, 0))
-
-    elif button == 5:
-        if action == "setup" or action == "released":
-            set_pixel(button, (0, 0, 255))
-        elif action == "pressed":
-            cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK)
-            set_pixel(button, (51, 153, 255))
-
+            set_button_pixel(button, (255, 0, 255))
+            kbd.send(Keycode.LEFT_CONTROL, Keycode.KEYPAD_PERIOD)
+    
     elif button == 6:
         if action == "setup" or action == "released":
-            set_pixel(button, (0, 255, 0))
+            set_button_pixel(button, (0, 0, 255))
         elif action == "pressed":
-            cc.send(ConsumerControlCode.PLAY_PAUSE)
-            set_pixel(button, (255, 0, 0))
+            cc.send(ConsumerControlCode.SCAN_PREVIOUS_TRACK)
+            set_button_pixel(button, (51, 153, 255))
 
     elif button == 7:
         if action == "setup" or action == "released":
-            set_pixel(button, (0, 0, 255))
+            set_button_pixel(button, (0, 255, 0))
+        elif action == "pressed":
+            cc.send(ConsumerControlCode.PLAY_PAUSE)
+            set_button_pixel(button, (255, 0, 0))
+
+    elif button == 8:
+        if action == "setup" or action == "released":
+            set_button_pixel(button, (0, 0, 255))
         elif action == "pressed":
             cc.send(ConsumerControlCode.SCAN_NEXT_TRACK)
-            set_pixel(button, (51, 153, 255))
-            
+            set_button_pixel(button, (51, 153, 255))
+    
+    elif button == 9:
+        if action == "setup" or action == "released":
+            set_button_pixel(button, (255, 102, 0))
+        elif action == "pressed":
+            cc.send(ConsumerControlCode.MUTE)
+            set_button_pixel(button, (255, 0, 0))
+    
+    elif button == 10:
+        if action == "setup" or action == "released":
+            set_button_pixel(button, (0, 255, 0))
+        elif action == "pressed":
+            cc.send(ConsumerControlCode.VOLUME_DECREMENT)
+            set_button_pixel(button, (255, 0, 0))
+
     elif button == 11:
-        if action == "tick":
-            if states["current"][11] == 0:
-                flash_button_example()
-            else:
-                rainbow_button_example()
-
-    elif button == 12:
         if action == "setup" or action == "released":
-            set_pixel(button, (0, 0, 255))
-            kbd.release(Keycode.SHIFT)
+            set_button_pixel(button, (0, 255, 0))
         elif action == "pressed":
-            set_pixel(button, (51, 153, 255))
-            kbd.press(Keycode.SHIFT)
+            cc.send(ConsumerControlCode.VOLUME_INCREMENT)
+            set_button_pixel(button, (255, 0, 0))
 
-    elif button == 15:
-        if action == "setup" or action == "released":
-            set_pixel(button, (0, 0, 255))
-        elif action == "pressed":
-            set_pixel(button, (255, 0, 255))
-            kbd.send(Keycode.LEFT_CONTROL, Keycode.KEYPAD_PERIOD)
 
+# Cleanly set pixel colour
+def set_button_pixel(button, colour):
+    pixels[pixel_map[button]] = colour
+
+# Cleanly clear pixel colour
+def clear_button_pixel(button):
+    pixels[pixel_map[button]] = (0, 0, 0)
+    
 while True:
     # Get the state right now
     states["current"] = button_states()
-    
-    hold_buttons = [2, 3, 11]
-    toggle_buttons = [12]  
+
+    hold_buttons = [10, 11]
+    toggle_buttons = [0]
 
     # Set up buttons
     # Probably a little overcomplicated, but allows a function to be mapped to button events
-    for i in range(0, 16):
+    for i in range(0, button_count):
         handle_button(
             i,
             hold=i in hold_buttons,
